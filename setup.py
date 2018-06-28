c5067b6381bdb6579ccbde1c9c5a5bbf129b2ff2
Aramís  Concepción Durán <aramiscd@gmail.com>
2018-06-28 22:07:01 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
 
 setup(
-    name="terminal_velocity",
-    version="0.1.10",
-    author="Sean Hammond, Vincent Perricone",
-    packages=["terminal_velocity"],
-    scripts=["bin/terminal_velocity"],
-    url="http://vhp.github.com/terminal_velocity/",
-    license="GNU General Public License, Version 3",
-    description="A fast note-taking app for the UNIX terminal",
-    long_description=open("README.md").read(),
+    name='terminal_velocity',
+    version='0.1.10',
+    author='Sean Hammond, Vincent Perricone, Aramís Concepción Durán',
+    packages=['terminal_velocity'],
+    scripts=['bin/terminal_velocity'],
+    url='github.com/aramiscd/terminal_velocity',
+    license='GNU General Public License, Version 3',
+    description='A fast note-taking app for the UNIX terminal',
+    long_description=open('README.md').read(),
     install_requires=[
-        "urwid==1.1.1",
-        "chardet==2.1.1",
+        'urwid==1.1.1',
+        'chardet==2.1.1',
         ],
     classifiers=[
         'Programming Language :: Python :: 2',
